fbe38529dd076a61f18a2583422e602b4335c607
Pengcheng Shi <shipc1220@gmail.com>
2024-07-07 16:25:09 +0800

    Update cbdice_loss.py

--- loss/cbdice_loss.py
+++ loss/cbdice_loss.py
@@ -3,44 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 from monai.transforms import distance_transform_edt
 from nnunetv2.training.loss.skeletonize import Skeletonize
-
-def soft_erode(img):
-    if len(img.shape) == 4:
-        p1 = -F.max_pool2d(-img, (3,1), (1,1), (1,0))
-        p2 = -F.max_pool2d(-img, (1,3), (1,1), (0,1))
-        return torch.min(p1,p2)
-    elif len(img.shape) == 5:
-        p1 = -F.max_pool3d(-img,(3,1,1),(1,1,1),(1,0,0))
-        p2 = -F.max_pool3d(-img,(1,3,1),(1,1,1),(0,1,0))
-        p3 = -F.max_pool3d(-img,(1,1,3),(1,1,1),(0,0,1))
-        return torch.min(torch.min(p1, p2), p3)
-
-def soft_dilate(img):
-    if len(img.shape) == 4:
-        return F.max_pool2d(img, (3,3), (1,1), (1,1))
-    elif len(img.shape) == 5:
-        return F.max_pool3d(img,(3,3,3),(1,1,1),(1,1,1))
-
-def soft_open(img):
-    return soft_dilate(soft_erode(img))
-
-def soft_skel(img, iter_):
-    img1 = soft_open(img)
-    skel = F.relu(img-img1)
-    for j in range(iter_):
-        img = soft_erode(img)
-        img1 = soft_open(img)
-        delta = F.relu(img - img1)
-        skel = skel + F.relu(delta-skel*delta)
-    return skel
+from nnunetv2.training.loss.soft_skeleton import SoftSkeletonize
 
 class CBDC_loss(torch.nn.Module):
     def __init__(self, iter_=3, smooth = 1.):
         super(CBDC_loss, self).__init__()
         self.iter = iter_
         self.smooth = smooth
-        self.skeletonization_module = Skeletonize(probabilistic=False, simple_point_detection='EulerCharacteristic')
-    
+        self.skeletonization_module = Skeletonize(probabilistic=False, simple_point_detection='EulerCharacteristic') # https://github.com/martinmenten/skeletonization-for-gradient-based-optimization
+        self.soft_skeletonize = SoftSkeletonize(num_iter=10) # https://github.com/jocpae/clDice/tree/master/cldice_loss/pytorch
+        
     def combine_tensors(self, A, B, C):
         A_C = A * C
         B_C = B * C
@@ -75,7 +47,7 @@ class CBDC_loss(torch.nn.Module):
             skel_1_R2_norm[skel != 1] = 0
             return dist_map_norm, skel_R_norm, skel_1_R2_norm
 
-    def forward(self, y_pred, y_true, skeletonization_flage=True):
+    def forward(self, y_pred, y_true, skeletonization_flage=False):
         if len(y_true.shape) == 4:
             dim = 2
         elif len(y_true.shape) == 5:
@@ -92,8 +64,8 @@ class CBDC_loss(torch.nn.Module):
             skel_pred = self.skeletonization_module(y_pred.unsqueeze(1)).squeeze(1)
             skel_true = self.skeletonization_module(y_true.unsqueeze(1).detach()).squeeze(1)
         else:
-            skel_pred = soft_skel(y_pred.unsqueeze(1), self.iter).squeeze(1)
-            skel_true = soft_skel(y_true.unsqueeze(1).detach(), self.iter).squeeze(1)
+            skel_pred = self.soft_skeletonize(y_pred.unsqueeze(1), self.iter).squeeze(1)
+            skel_true = self.soft_skeletonize(y_true.unsqueeze(1).detach(), self.iter).squeeze(1)
 
         q_vl, q_slvl, q_sl = self.get_weights(y_true, skel_true, dim)
         q_vp, q_spvp, q_sp = self.get_weights(y_pred, skel_pred, dim)
@@ -110,7 +82,8 @@ class clMdice_loss(torch.nn.Module):
         self.iter = iter_
         self.smooth = smooth
         self.skeletonization_module = Skeletonize(probabilistic=False, simple_point_detection='EulerCharacteristic')
-    
+        self.soft_skeletonize = SoftSkeletonize(num_iter=10)
+        
     def combine_tensors(self, A, B, C):
         A_C = A * C
         B_C = B * C
@@ -145,7 +118,7 @@ class clMdice_loss(torch.nn.Module):
             skel_1_R2_norm[skel != 1] = 0
             return dist_map_norm, skel_R_norm, skel_1_R2_norm
 
-    def forward(self, y_pred, y_true, skeletonization_flage=True):
+    def forward(self, y_pred, y_true, skeletonization_flage=False):
         if len(y_true.shape) == 4:
             dim = 2
         elif len(y_true.shape) == 5:
@@ -162,8 +135,8 @@ class clMdice_loss(torch.nn.Module):
             skel_pred = self.skeletonization_module(y_pred.unsqueeze(1)).squeeze(1)
             skel_true = self.skeletonization_module(y_true.unsqueeze(1).detach()).squeeze(1)
         else:
-            skel_pred = soft_skel(y_pred.unsqueeze(1), self.iter).squeeze(1)
-            skel_true = soft_skel(y_true.unsqueeze(1).detach(), self.iter).squeeze(1)
+            skel_pred = self.soft_skeletonize(y_pred.unsqueeze(1), self.iter).squeeze(1)
+            skel_true = self.soft_skeletonize(y_true.unsqueeze(1).detach(), self.iter).squeeze(1)
 
         q_vl, q_slvl, _ = self.get_weights(y_true, skel_true, dim)
         q_vp, q_spvp, _ = self.get_weights(y_pred, skel_pred, dim)
