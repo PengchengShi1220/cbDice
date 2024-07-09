9702a8ab5e6bcc8f567be684bbe9e0e11e26415b
Pengcheng Shi <shipc1220@gmail.com>
2024-07-09 15:03:35 +0800

    Update cldice_loss.py

--- loss/cldice_loss.py
+++ loss/cldice_loss.py
@@ -1,56 +1,44 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from nnunetv2.training.loss.skeletonize import Skeletonize
+from nnunetv2.training.loss.soft_skeleton import SoftSkeletonize
 
-
-def soft_erode(img):
-    if len(img.shape)==4:
-        p1 = -F.max_pool2d(-img, (3,1), (1,1), (1,0))
-        p2 = -F.max_pool2d(-img, (1,3), (1,1), (0,1))
-        return torch.min(p1,p2)
-    elif len(img.shape)==5:
-        p1 = -F.max_pool3d(-img,(3,1,1),(1,1,1),(1,0,0))
-        p2 = -F.max_pool3d(-img,(1,3,1),(1,1,1),(0,1,0))
-        p3 = -F.max_pool3d(-img,(1,1,3),(1,1,1),(0,0,1))
-        return torch.min(torch.min(p1, p2), p3)
-
-
-def soft_dilate(img):
-    if len(img.shape)==4:
-        return F.max_pool2d(img, (3,3), (1,1), (1,1))
-    elif len(img.shape)==5:
-        return F.max_pool3d(img,(3,3,3),(1,1,1),(1,1,1))
-
-def soft_open(img):
-    return soft_dilate(soft_erode(img))
-
-def soft_skel(img, iter_):
-    img1 = soft_open(img)
-    skel = F.relu(img-img1)
-    for j in range(iter_):
-        img = soft_erode(img)
-        img1 = soft_open(img)
-        delta = F.relu(img - img1)
-        skel = skel + F.relu(delta-skel*delta)
-    return skel
-    
 class CLDC_loss(torch.nn.Module):
-    def __init__(self, iter_=3, smooth = 1.):
+    def __init__(self, iter_=10, smooth = 1.):
         super(CLDC_loss, self).__init__()
-        self.iter = iter_
         self.smooth = smooth
+        
+        # Topology-preserving skeletonization: https://github.com/martinmenten/skeletonization-for-gradient-based-optimization
+        self.t_skeletonize = Skeletonize(probabilistic=False, simple_point_detection='EulerCharacteristic')
+        
+        # Morphological skeletonization: https://github.com/jocpae/clDice/tree/master/cldice_loss/pytorch
+        self.m_skeletonize = SoftSkeletonize(num_iter=iter_)
 
-    def forward(self, y_pred, y_true):
-        y_prob = torch.softmax(y_pred, 1)
-        y_pre = torch.argmax(y_prob, dim=1)
-        y_pred = torch.where(y_pre > 0, 1, 0).float().unsqueeze(1)
-        y_true = torch.where(y_true > 0, 1, 0).float()
+    def forward(self, y_pred, y_true, t_skeletonize_flage=False):
+        if len(y_true.shape) == 4:
+            dim = 2
+        elif len(y_true.shape) == 5:
+            dim = 3
+        else:
+            raise ValueError("y_true should be 4D or 5D tensor.")
 
-        skel_pred = soft_skel(y_pred, self.iter)
-        skel_true = soft_skel(y_true.detach(), self.iter)
+        y_pred_fore = y_pred[:, 1:]
+        y_pred_fore = torch.max(y_pred_fore, dim=1, keepdim=True)[0] # C foreground channels -> 1 channel
+        y_pred_binary = torch.cat([y_pred[:, :1], y_pred_fore], dim=1)
+        y_prob_binary = torch.softmax(y_pred_binary, 1)
+        y_pred = torch.where(y_prob_binary[:, 1] > 0.5, 1, 0).float()
+        y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float()
+
+        if t_skeletonize_flage:
+            skel_pred = self.t_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
+            skel_true = self.t_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
+        else:
+            skel_pred = self.m_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
+            skel_true = self.m_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
+
         tprec = (torch.sum(torch.multiply(skel_pred, y_true))+self.smooth)/(torch.sum(skel_pred)+self.smooth)    
         tsens = (torch.sum(torch.multiply(skel_true, y_pred))+self.smooth)/(torch.sum(skel_true)+self.smooth)    
         cl_dice = 1.- 2.0 * (tprec*tsens)/(tprec+tsens)
-        
+
         return cl_dice
-    
