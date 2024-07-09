89d69c421049239bd991ee5d8c7dbb737e448e1f
Pengcheng Shi <shipc1220@gmail.com>
2024-07-09 19:30:49 +0800

    Add files via upload

---
+++ loss/b_dou_loss.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+
+class BoundaryDoULoss(nn.Module):
+    def __init__(self, n_classes):
+        super(BoundaryDoULoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i
+            tensor_list.append(temp_prob.unsqueeze(1))
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _adaptive_size(self, score, target):
+        kernel = torch.Tensor([[0,1,0], [1,1,1], [0,1,0]])
+        padding_out = torch.zeros((target.shape[0], target.shape[-2]+2, target.shape[-1]+2))
+        padding_out[:, 1:-1, 1:-1] = target
+        h, w = 3, 3
+
+        Y = torch.zeros((padding_out.shape[0], padding_out.shape[1] - h + 1, padding_out.shape[2] - w + 1)).cuda()
+        for i in range(Y.shape[0]):
+            Y[i, :, :] = torch.conv2d(target[i].unsqueeze(0).unsqueeze(0), kernel.unsqueeze(0).unsqueeze(0).cuda(), padding=1)
+        Y = Y * target
+        Y[Y == 5] = 0
+        C = torch.count_nonzero(Y)
+        S = torch.count_nonzero(target)
+        smooth = 1e-5
+        alpha = 1 - (C + smooth) / (S + smooth)
+        alpha = 2 * alpha - 1
+
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        alpha = min(alpha, 0.8)  ## We recommend using a truncated alpha of 0.8, as using truncation gives better results on some datasets and has rarely effect on others.
+        loss = (z_sum + y_sum - 2 * intersect + smooth) / (z_sum + y_sum - (1 + alpha) * intersect + smooth)
+
+        return loss
+
+    def forward(self, inputs, target):
+        inputs = torch.softmax(inputs, dim=1)
+        target = self._one_hot_encoder(target.squeeze(1))
+
+        assert inputs.size() == target.size(), 'predict {} & target {} shape do not match'.format(inputs.size(), target.size())
+
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            loss += self._adaptive_size(inputs[:, i], target[:, i])
+        return loss / self.n_classes
+    
