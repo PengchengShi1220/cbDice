380716360c95f1c3d862e3700a9f906fb2ceb389
Pengcheng Shi <shipc1220@gmail.com>
2024-07-11 17:39:31 +0800

    Update cbdice_loss.py

--- loss/cbdice_loss.py
+++ loss/cbdice_loss.py
@@ -10,43 +10,10 @@ class CBDC_loss(torch.nn.Module):
         
         # Topology-preserving skeletonization: https://github.com/martinmenten/skeletonization-for-gradient-based-optimization
         self.t_skeletonize = Skeletonize(probabilistic=False, simple_point_detection='EulerCharacteristic')
+        self.t_skeletonize_prob = Skeletonize(probabilistic=True, simple_point_detection='EulerCharacteristic')
         
         # Morphological skeletonization: https://github.com/jocpae/clDice/tree/master/cldice_loss/pytorch
         self.m_skeletonize = SoftSkeletonize(num_iter=iter_)
-        
-    def combine_tensors(self, A, B, C):
-        A_C = A * C
-        B_C = B * C
-        D = B_C.clone()
-        mask_AC = (A != 0) & (B == 0)
-        D[mask_AC] = A_C[mask_AC]
-        return D
-
-    def get_weights(self, mask, skel, dim):
-        distances = distance_transform_edt(mask).float()
-
-        smooth = 1e-7
-        mask_inv = mask == 0
-        distances[mask_inv] = 0
-
-        skel_radius = torch.zeros_like(distances, dtype=torch.float32)
-        skel_radius[skel == 1] = distances[skel == 1]
-
-        skel_radius_max = skel_radius.reshape(skel_radius.shape[0], -1).max(dim=1).values.view(-1, 1, 1, 1)
-        valid_max_mask = skel_radius_max > 0
-        skel_radius_max[~valid_max_mask] = 1
-
-        dist_map_norm = torch.where(valid_max_mask, distances / skel_radius_max, distances)
-        skel_R_norm = torch.where(valid_max_mask, skel_radius / skel_radius_max, skel_radius)
-
-        if dim == 2:
-            skel_1_R_norm = (1 + smooth) / (skel_R_norm + smooth)
-            skel_1_R_norm[skel != 1] = 0
-            return dist_map_norm, skel_R_norm, skel_1_R_norm
-        else:
-            skel_1_R2_norm = (1 + smooth) / (skel_R_norm ** 2 + smooth)
-            skel_1_R2_norm[skel != 1] = 0
-            return dist_map_norm, skel_R_norm, skel_1_R2_norm
 
     def forward(self, y_pred, y_true, t_skeletonize_flage=False):
         if len(y_true.shape) == 4:
@@ -60,23 +27,24 @@ class CBDC_loss(torch.nn.Module):
         y_pred_fore = torch.max(y_pred_fore, dim=1, keepdim=True)[0] # C foreground channels -> 1 channel
         y_pred_binary = torch.cat([y_pred[:, :1], y_pred_fore], dim=1)
         y_prob_binary = torch.softmax(y_pred_binary, 1)
-        y_pred = torch.where(y_prob_binary[:, 1] > 0.5, 1, 0).float()
-        y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float()
+        y_pred_prob = y_prob_binary[:, 1] # predicted probability map of foreground
+        y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float() # ground truth of foreground
 
         if t_skeletonize_flage:
-            skel_pred = self.t_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
-            skel_true = self.t_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
+            skel_pred_prob = self.t_skeletonize_prob(y_pred_prob.unsqueeze(1)).squeeze(1)
+            skel_true = self.t_skeletonize(y_true.unsqueeze(1)).squeeze(1)
         else:
-            skel_pred = self.m_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
-            skel_true = self.m_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
+            skel_pred_prob = self.m_skeletonize(y_pred_prob.unsqueeze(1)).squeeze(1)
+            skel_true = self.m_skeletonize(y_true.unsqueeze(1)).squeeze(1)
 
-        q_vl, q_slvl, q_sl = self.get_weights(y_true, skel_true, dim)
-        q_vp, q_spvp, q_sp = self.get_weights(y_pred, skel_pred, dim)
+        q_vl, q_slvl, q_sl = get_weights(y_true, skel_true, dim, prob_flag=False)
+        q_vp, q_spvp, q_sp = get_weights(y_pred_prob, skel_pred_prob, dim, prob_flag=True)
 
-        w_tprec = (torch.sum(torch.multiply(q_sp, q_vl))+self.smooth)/(torch.sum(self.combine_tensors(q_spvp, q_slvl, q_sp))+self.smooth)
-        w_tsens = (torch.sum(torch.multiply(q_sl, q_vp))+self.smooth)/(torch.sum(self.combine_tensors(q_slvl, q_spvp, q_sl))+self.smooth)
+        w_tprec = (torch.sum(torch.multiply(q_sp, q_vl))+self.smooth)/(torch.sum(combine_tensors(q_spvp, q_slvl, q_sp))+self.smooth)
+        w_tsens = (torch.sum(torch.multiply(q_sl, q_vp))+self.smooth)/(torch.sum(combine_tensors(q_slvl, q_spvp, q_sl))+self.smooth)
+
         cb_dice = 1. - 2.0 * (w_tprec * w_tsens) / (w_tprec + w_tsens)
-
+        print("cb_dice: ", cb_dice)
         return cb_dice
     
 class clMdice_loss(torch.nn.Module):
@@ -89,40 +57,6 @@ class clMdice_loss(torch.nn.Module):
         
         # Morphological skeletonization: https://github.com/jocpae/clDice/tree/master/cldice_loss/pytorch
         self.m_skeletonize = SoftSkeletonize(num_iter=iter_)
-        
-    def combine_tensors(self, A, B, C):
-        A_C = A * C
-        B_C = B * C
-        D = B_C.clone()
-        mask_AC = (A != 0) & (B == 0)
-        D[mask_AC] = A_C[mask_AC]
-        return D
-
-    def get_weights(self, mask, skel, dim):
-        distances = distance_transform_edt(mask).float()
-
-        smooth = 1e-7
-        mask_inv = mask == 0
-        distances[mask_inv] = 0
-
-        skel_radius = torch.zeros_like(distances, dtype=torch.float32)
-        skel_radius[skel == 1] = distances[skel == 1]
-
-        skel_radius_max = skel_radius.reshape(skel_radius.shape[0], -1).max(dim=1).values.view(-1, 1, 1, 1)
-        valid_max_mask = skel_radius_max > 0
-        skel_radius_max[~valid_max_mask] = 1
-
-        dist_map_norm = torch.where(valid_max_mask, distances / skel_radius_max, distances)
-        skel_R_norm = torch.where(valid_max_mask, skel_radius / skel_radius_max, skel_radius)
-
-        if dim == 2:
-            skel_1_R_norm = (1 + smooth) / (skel_R_norm + smooth)
-            skel_1_R_norm[skel != 1] = 0
-            return dist_map_norm, skel_R_norm, skel_1_R_norm
-        else:
-            skel_1_R2_norm = (1 + smooth) / (skel_R_norm ** 2 + smooth)
-            skel_1_R2_norm[skel != 1] = 0
-            return dist_map_norm, skel_R_norm, skel_1_R2_norm
 
     def forward(self, y_pred, y_true, t_skeletonize_flage=False):
         if len(y_true.shape) == 4:
@@ -136,24 +70,75 @@ class clMdice_loss(torch.nn.Module):
         y_pred_fore = torch.max(y_pred_fore, dim=1, keepdim=True)[0] # C foreground channels -> 1 channel
         y_pred_binary = torch.cat([y_pred[:, :1], y_pred_fore], dim=1)
         y_prob_binary = torch.softmax(y_pred_binary, 1)
-        y_pred = torch.where(y_prob_binary[:, 1] > 0.5, 1, 0).float()
-        y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float()
+        y_pred_prob = y_prob_binary[:, 1] # predicted probability map of foreground
+        y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float() # ground truth of foreground
 
         if t_skeletonize_flage:
-            skel_pred = self.t_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
-            skel_true = self.t_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
+            skel_pred_prob = self.t_skeletonize_prob(y_pred_prob.unsqueeze(1)).squeeze(1)
+            skel_true = self.t_skeletonize(y_true.unsqueeze(1)).squeeze(1)
         else:
-            skel_pred = self.m_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
-            skel_true = self.m_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
+            skel_pred_prob = self.m_skeletonize(y_pred_prob.unsqueeze(1)).squeeze(1)
+            skel_true = self.m_skeletonize(y_true.unsqueeze(1)).squeeze(1)
 
-        q_vl, q_slvl, _ = self.get_weights(y_true, skel_true, dim)
-        q_vp, q_spvp, _ = self.get_weights(y_pred, skel_pred, dim)
+        q_vl, q_slvl, _ = get_weights(y_true, skel_true, dim, prob_flag=False)
+        q_vp, q_spvp, _ = get_weights(y_pred_prob, skel_pred_prob, dim, prob_flag=True)
 
         q_sl = skel_true
-        q_sp = skel_pred
+        q_sp = skel_pred_prob
 
-        w_tprec = (torch.sum(torch.multiply(q_sp, q_vl))+self.smooth)/(torch.sum(self.combine_tensors(q_spvp, q_slvl, q_sp))+self.smooth)
-        w_tsens = (torch.sum(torch.multiply(q_sl, q_vp))+self.smooth)/(torch.sum(self.combine_tensors(q_slvl, q_spvp, q_sl))+self.smooth)
+        w_tprec = (torch.sum(torch.multiply(q_sp, q_vl))+self.smooth)/(torch.sum(combine_tensors(q_spvp, q_slvl, q_sp))+self.smooth)
+        w_tsens = (torch.sum(torch.multiply(q_sl, q_vp))+self.smooth)/(torch.sum(combine_tensors(q_slvl, q_spvp, q_sl))+self.smooth)
+
         cl_m_dice = 1. - 2.0 * (w_tprec * w_tsens) / (w_tprec + w_tsens)
-        
+
         return cl_m_dice
+
+def combine_tensors(A, B, C):
+    A_C = A * C
+    B_C = B * C
+    D = B_C.clone()
+    mask_AC = (A != 0) & (B == 0)
+    D[mask_AC] = A_C[mask_AC]
+    return D
+
+def get_weights(mask_input, skel_input, dim, prob_flag=True):
+    if prob_flag:
+        mask_prob = mask_input
+        skel_prob = skel_input
+
+        mask = (mask_prob > 0.5).int()
+        skel = (skel_prob > 0.5).int()
+    else:
+        mask = mask_input
+        skel = skel_input
+
+    distances = distance_transform_edt(mask).float()
+
+    smooth = 1e-7
+    distances[mask == 0] = 0
+
+    skel_radius = torch.zeros_like(distances, dtype=torch.float32)
+    skel_radius[skel == 1] = distances[skel == 1]
+
+    dist_map_norm = torch.zeros_like(distances, dtype=torch.float32)
+    skel_R_norm = torch.zeros_like(skel_radius, dtype=torch.float32)
+    for i in range(skel_radius.shape[0]):
+        distances_i = distances[i]
+        skel_i = skel_radius[i]
+        skel_radius_max = max(skel_i.max(), 1)
+    
+        distances_i[distances_i > skel_radius_max] = skel_radius_max
+        dist_map_norm[i] = distances_i / skel_radius_max
+        skel_R_norm[i] = skel_i / skel_radius_max
+
+    if dim == 2:
+        I_norm = (1 + smooth) / (skel_R_norm + smooth) # weight for skel
+    else:
+        I_norm = (1 + smooth) / (skel_R_norm ** 2 + smooth)
+
+    I_norm[skel == 0] = 0 # 0 for non-skeleton pixels
+
+    if prob_flag:
+        return dist_map_norm * mask_prob, skel_R_norm * skel_prob, I_norm * skel_prob
+    else:
+        return dist_map_norm * mask, skel_R_norm * skel, I_norm * skel
