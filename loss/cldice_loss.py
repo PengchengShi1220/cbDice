cb31d830ca7646f08f8ffe18710aa0160f525651
Pengcheng Shi <shipc1220@gmail.com>
2024-07-11 17:39:50 +0800

    Update cldice_loss.py

--- loss/cldice_loss.py
+++ loss/cldice_loss.py
@@ -9,6 +9,7 @@ class CLDC_loss(torch.nn.Module):
         
         # Topology-preserving skeletonization: https://github.com/martinmenten/skeletonization-for-gradient-based-optimization
         self.t_skeletonize = Skeletonize(probabilistic=False, simple_point_detection='EulerCharacteristic')
+        self.t_skeletonize_prob = Skeletonize(probabilistic=True, simple_point_detection='EulerCharacteristic')
         
         # Morphological skeletonization: https://github.com/jocpae/clDice/tree/master/cldice_loss/pytorch
         self.m_skeletonize = SoftSkeletonize(num_iter=iter_)
@@ -19,18 +20,18 @@ class CLDC_loss(torch.nn.Module):
         y_pred_fore = torch.max(y_pred_fore, dim=1, keepdim=True)[0] # C foreground channels -> 1 channel
         y_pred_binary = torch.cat([y_pred[:, :1], y_pred_fore], dim=1)
         y_prob_binary = torch.softmax(y_pred_binary, 1)
-        y_pred = torch.where(y_prob_binary[:, 1] > 0.5, 1, 0).float()
+        y_pred_prob = y_prob_binary[:, 1]
         y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float()
 
         if t_skeletonize_flage:
-            skel_pred = self.t_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
+            skel_pred_prob = self.t_skeletonize_prob(y_pred_prob.unsqueeze(1)).squeeze(1)
             skel_true = self.t_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
         else:
-            skel_pred = self.m_skeletonize(y_pred.unsqueeze(1)).squeeze(1)
+            skel_pred_prob = self.m_skeletonize(y_pred_prob.unsqueeze(1)).squeeze(1)
             skel_true = self.m_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
 
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true))+self.smooth)/(torch.sum(skel_pred)+self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred))+self.smooth)/(torch.sum(skel_true)+self.smooth)    
+        tprec = (torch.sum(torch.multiply(skel_pred_prob, y_true))+self.smooth)/(torch.sum(skel_pred_prob)+self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred_prob))+self.smooth)/(torch.sum(skel_true)+self.smooth)
         cl_dice = 1.- 2.0 * (tprec*tsens)/(tprec+tsens)
 
         return cl_dice
