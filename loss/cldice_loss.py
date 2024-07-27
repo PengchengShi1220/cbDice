0e4f7561826b95f91bf93c808ed7495a0c0a6ffc
Pengcheng Shi <shipc1220@gmail.com>
2024-07-28 01:12:08 +0800

    Update cldice_loss.py

--- loss/cldice_loss.py
+++ loss/cldice_loss.py
@@ -9,7 +9,6 @@ class SoftclDiceLoss(torch.nn.Module):
         
         # Topology-preserving skeletonization: https://github.com/martinmenten/skeletonization-for-gradient-based-optimization
         self.t_skeletonize = Skeletonize(probabilistic=False, simple_point_detection='EulerCharacteristic')
-        self.t_skeletonize_prob = Skeletonize(probabilistic=True, simple_point_detection='EulerCharacteristic')
         
         # Morphological skeletonization: https://github.com/jocpae/clDice/tree/master/cldice_loss/pytorch
         self.m_skeletonize = SoftSkeletonize(num_iter=iter_)
@@ -21,14 +20,19 @@ class SoftclDiceLoss(torch.nn.Module):
         y_pred_binary = torch.cat([y_pred[:, :1], y_pred_fore], dim=1)
         y_prob_binary = torch.softmax(y_pred_binary, 1)
         y_pred_prob = y_prob_binary[:, 1]
-        y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float()
 
-        if t_skeletonize_flage:
-            skel_pred_prob = self.t_skeletonize_prob(y_pred_prob.unsqueeze(1)).squeeze(1)
-            skel_true = self.t_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
-        else:
-            skel_pred_prob = self.m_skeletonize(y_pred_prob.unsqueeze(1)).squeeze(1)
-            skel_true = self.m_skeletonize(y_true.unsqueeze(1).detach()).squeeze(1)
+        with torch.no_grad():
+            y_true = torch.where(y_true > 0, 1, 0).squeeze(1).float() # ground truth of foreground
+            y_pred_hard = (y_pred_prob > 0.5).float()
+        
+            if t_skeletonize_flage:
+                skel_pred_hard = self.t_skeletonize(y_pred_hard.unsqueeze(1)).squeeze(1)
+                skel_true = self.t_skeletonize(y_true.unsqueeze(1)).squeeze(1)
+            else:
+                skel_pred_hard = self.m_skeletonize(y_pred_hard.unsqueeze(1)).squeeze(1)
+                skel_true = self.m_skeletonize(y_true.unsqueeze(1)).squeeze(1)
+
+        skel_pred_prob = skel_pred_hard * y_pred_prob
 
         tprec = (torch.sum(torch.multiply(skel_pred_prob, y_true))+self.smooth)/(torch.sum(skel_pred_prob)+self.smooth)    
         tsens = (torch.sum(torch.multiply(skel_true, y_pred_prob))+self.smooth)/(torch.sum(skel_true)+self.smooth)
